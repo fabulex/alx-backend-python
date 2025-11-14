7ffb326ce2e7ee221aee5ba389d0fe10df34d5be
@fabulex <famuhjo@gmail.com>
2025-11-14 21:11:03 +0300

    add

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,8 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +28,26 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(context.exception.args[0], path[-1])
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests for get_json function"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Tests get_json returns expected payload without HTTP calls."""
+        mock_resp = Mock()
+        mock_resp.json.return_value = test_payload
+        mock_get.return_value = mock_resp
+
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_once_with(test_url)
+
+
+if __name__ == '__main__':
+    unittest.main()
