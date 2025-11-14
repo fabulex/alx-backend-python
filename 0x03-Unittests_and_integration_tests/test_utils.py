3dfcf2f505b51112a8816729cbe5d81ba472cc7e
@fabulex <famuhjo@gmail.com>
2025-11-14 21:57:06 +0300

    add

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -63,7 +63,7 @@ class TestMemoize(unittest.TestCase):
                 return self.a_method()
 
         with patch.object(
-            TestClass, 'a_method',return_value=42
+            TestClass, 'a_method', return_value=42
         ) as mock_method:
             test_obj = TestClass()
             # Call property twice
