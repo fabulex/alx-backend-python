bc2f192eaec79829907fc3063dec0b3e95697ebc
FM <famuhjo@gmail.com>
2025-12-09 19:08:14 +0300

    add

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', 'messaging_app_db'),
         'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'Sxeteaact2p9'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
